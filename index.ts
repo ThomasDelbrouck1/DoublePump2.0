d8002e433667935343fe4c797902358ffc7eec7f
Thomas Delbrouck <s130142@ap.be>
2024-04-06 15:58:52 +0000

    express conversion complete

---
+++ index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('port', 3000);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/avatar', (req, res) => {
+    res.render('avatar');
+});
+app.get('/favoriete', (req, res) => {
+    res.render('favoriete');
+});
+app.get('/registratiepagina', (req, res) => {
+    res.render('registratiepagina');
+});
+app.get('/zwartelijst', (req, res) => {
+    res.render('zwartelijst');
+});
+
+app.listen(app.get('port'), async () => {
+    console.log('[server] http://localhost/:' + app.get('port'));
+});
+
