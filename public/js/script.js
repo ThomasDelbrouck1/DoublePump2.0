375621823ccdb34c3d6b5ffd899425d8ec3ec12c
Sofiane Hammich <sofiane.hammich@hotmail.com>
2024-04-23 19:33:15 +0000

    modal changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,67 +1,39 @@
 // wanneer ze 20px zakt komt e button tevoorschijn
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 
 //klik om naar boven te gaan
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 //Avatar
 
-
 //POP UP
-document.addEventListener('DOMContentLoaded', function () {
-    const gameBlocks = document.querySelectorAll('.game');
-    const popup = document.getElementById('popup');
-    const closeBtn = document.querySelector('.close-btn');
+document.addEventListener("DOMContentLoaded", function () {
+  const gameBlocks = document.querySelectorAll(".game");
+  const popup = document.getElementById("popup");
+  const closeBtn = document.querySelector(".close-btn");
 
-
-    gameBlocks.forEach((block) => {
-
-        block.addEventListener('click', () => {
-            if (block.id !== 'fortnite') {
-                popup.style.display = 'block';
-            }
-        });
+  gameBlocks.forEach((block) => {
+    block.addEventListener("click", () => {
+      if (block.id !== "fortnite") {
+        popup.style.display = "block";
+      }
     });
+  });
 
-    closeBtn.addEventListener('click', () => {
-        popup.style.display = 'none';
-    });
+  closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
 });
-
-
-
-
-
-/*let fortnite = document.getElementById('fortnite');
-
-fortnite.addEventListener('click', function () {
-    // Plaats hier je JavaScript-code
-    var modal = document.getElementById("myModal");
-    var img = document.getElementById("fortimg");
-
-    function openModal() {
-        modal.style.display = "block";
-    }
-
-    function closeModal() {
-        modal.style.display = "none";
-    }
-
-    // Sluit het modale venster wanneer er buiten geklikt wordt
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            closeModal();
-        }
-    }
-});*/
