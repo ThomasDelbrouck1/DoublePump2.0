216599442dea3d4de71e8f0b5f79cd956aae1db5
Sofiane Hammich <sofiane.hammich@hotmail.com>
2024-05-07 12:36:26 +0000

    registreer pagina

--- public/js/script.js
+++ public/js/script.js
@@ -36,3 +36,15 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const animations = ['moveDiagonal', 'moveHorizontal', 'moveVertical'];
+  const elements = document.querySelectorAll('.floatingElement');
+
+  elements.forEach(el => {
+    const animationName = animations[Math.floor(Math.random() * animations.length)];
+    const duration = Math.random() * 10 + 15;
+    el.style.animationName = animationName;
+    el.style.animationDuration = `${duration}s`;
+  });
+});
